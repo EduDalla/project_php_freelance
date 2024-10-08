231d75786428d3f63d2e1e959fe1890b0201cc02
EduDalla <62569881+EduDalla@users.noreply.github.com>
2024-10-08 18:45:19 -0300

    Criação de primeira rotas

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+
+    <a href="{{ route('projects.show', 1)}}">
+        Primero projeto
+    </a>
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::view('/', 'projects.index');
+
+Route::view('/projects/{project}', 'projects.show')->name('projects.show');
+
 
